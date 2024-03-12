9bc998857d1c1c5228e6be3b81d1f287fb22ec4d
skpark33 <skpark@sqisoft.com>
2024-03-12 16:10:45 +0900

    3

--- lib/common/creta_vars.dart
+++ lib/common/creta_vars.dart
@@ -1,9 +1,10 @@
 import 'dart:ui';
+import '../model/app_enums.dart';
 
 class CretaVars {
+  static ServiceType serviceType = ServiceType.signage;
+
   static bool isCanvaskit = true;
   static bool isDeveloper = false;
-
   static Size displaySize = Size.zero;
-
 }
