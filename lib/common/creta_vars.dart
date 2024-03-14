56685430932f9063446b1a2b9c576a14eb54373e
skpark33 <skpark@sqisoft.com>
2024-03-14 20:43:29 +0900

    1

--- lib/common/creta_vars.dart
+++ lib/common/creta_vars.dart
@@ -7,4 +7,25 @@ class CretaVars {
   static bool isCanvaskit = true;
   static bool isDeveloper = false;
   static Size displaySize = Size.zero;
+
+  static BookType defaultBookType() {
+    switch (CretaVars.serviceType) {
+      case ServiceType.signage:
+        return BookType.signage;
+      case ServiceType.digitalBarricade:
+        return BookType.digitalBarricade;
+      case ServiceType.presentaion:
+        return BookType.presentaion;
+      case ServiceType.escalator:
+        return BookType.escalator;
+      case ServiceType.board:
+        return BookType.board;
+      case ServiceType.cdu:
+        return BookType.cdu;
+      case ServiceType.etc:
+        return BookType.etc;
+      default:
+        return BookType.signage;
+    }
+  }
 }
