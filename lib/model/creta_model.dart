21709871dd92212a19dc4a8002c0311b9b1c06e6
skpark33 <skpark@sqisoft.com>
2024-06-13 20:32:29 +0900

    1

--- lib/model/creta_model.dart
+++ lib/model/creta_model.dart
@@ -30,8 +30,52 @@ class CretaModel extends AbsExModel {
       if (lineCount > 0) {
         retval += ',\n';
       }
-      if (entry.value is bool || entry.value is int || entry.value is double) {
+
+      if (entry.value is bool || entry.value is int) {
         retval += '$tab\t"${entry.key}": ${entry.value}';
+      } else if (entry.value is double) {
+        // . 이 있는지 확인후.
+        String value = '${entry.value}';
+        if (value.contains('.')) {
+          retval += '$tab\t"${entry.key}": $value';
+        } else {
+          retval += '$tab\t"${entry.key}": $value.0';
+        }
+      } else if (entry.value is List<String>) {
+        retval += '$tab\t"${entry.key}": [';
+        List<String> list = entry.value;
+        for (int i = 0; i < list.length; i++) {
+          if (i > 0) {
+            retval += ', ';
+          }
+          retval += '"${list[i]}"';
+        }
+        retval += ']';
+      } else if (entry.value is List<int> || entry.value is List<bool>) {
+        retval += '$tab\t"${entry.key}": [';
+        List<int> list = entry.value;
+        for (int i = 0; i < list.length; i++) {
+          if (i > 0) {
+            retval += ', ';
+          }
+          retval += '${list[i]}';
+        }
+        retval += ']';
+      } else if (entry.value is List<double>) {
+        retval += '$tab\t"${entry.key}": [';
+        List<double> list = entry.value;
+        for (int i = 0; i < list.length; i++) {
+          if (i > 0) {
+            retval += ', ';
+          }
+          String value = '${list[i]}';
+          if (value.contains('.')) {
+            retval += value;
+          } else {
+            retval += '$value.0';
+          }
+        }
+        retval += ']';
       } else {
         String value = entry.value.toString().replaceAll('"', '\\"').replaceAll('\n', ' ');
         // .replaceAll(']', '\\]')
