c2bbb30a30bcc6ba637d98267874b3063ba7a252
박기순 <50977788+kspark-sqisoft@users.noreply.github.com>
2024-11-04 16:53:32 +0900

    주석 추가

--- lib/common/utils/save_log_file_io.dart
+++ lib/common/utils/save_log_file_io.dart
@@ -1,6 +1,7 @@
 class SaveLogFile {
   static void saveLogToFile(String logData, String outFileName,
       {String folder = "download"}) {
+    //IO 환경에서 로그 파일 저장 하는 정의 미정
     throw UnimplementedError();
   }
 }
