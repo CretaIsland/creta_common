c4c8ca19470c6962468790c2fabaf9decde25b26
박기순 <50977788+kspark-sqisoft@users.noreply.github.com>
2024-11-06 10:25:21 +0900

    io 파일 저정

--- lib/common/save_log_to_file_io.dart
+++ lib/common/save_log_to_file_io.dart
@@ -1,6 +1,18 @@
-import 'dart:io' as io;
+import 'dart:io';
 
 void saveLogToFile(String logData, String outFileName,
     {String folder = "download"}) {
   print('save log to file io');
+  final directory = Directory(folder);
+  if (!directory.existsSync()) {
+    directory.createSync(recursive: true); // 폴더가 없을 경우 생성
+  }
+
+  final filePath = '${directory.path}/$outFileName';
+  final file = File(filePath);
+
+  // 파일 쓰기
+  file.writeAsStringSync(logData);
+
+  print('Log saved to $filePath');
 }
--- lib/common/save_log_to_file_web.dart
+++ lib/common/save_log_to_file_web.dart
@@ -1,12 +1,12 @@
-import 'dart:html' as html;
+import 'dart:html';
 
 void saveLogToFile(String logData, String outFileName,
     {String folder = "download"}) {
   print('save log to file web');
-  final blob = html.Blob([logData]);
-  final url = html.Url.createObjectUrlFromBlob(blob);
-  html.AnchorElement(href: url)
+  final blob = Blob([logData]);
+  final url = Url.createObjectUrlFromBlob(blob);
+  AnchorElement(href: url)
     ..setAttribute(folder, outFileName)
     ..click();
-  html.Url.revokeObjectUrl(url);
+  Url.revokeObjectUrl(url);
 }
