cb313e6070304a31036fb80c3a419bd2902ae7b7
skpark33 <skpark33333@gmail.com>
2024-11-25 15:24:46 +0900

    0.1.1

--- lib/common/save_log_to_file_io.dart
+++ lib/common/save_log_to_file_io.dart
@@ -19,3 +19,23 @@ void saveLogToFile(String logData, String outFileName, {String folder = "downloa
 Future<List<int>> extractBytesFromFile(dynamic param) async {
   throw UnsupportedError('extractBytesFromFile is not supported on this platform');
 }
+
+Future<void> downloadImage(String urlImage, String bucketId, String mediaApiUrl) async {
+  throw UnsupportedError('downloadImage is not supported on this platform');
+  // http
+  //     .post(Uri.parse("$mediaApiUrl/downloadAiImg"),
+  //         headers: {"Content-type": "application/json"},
+  //         body: jsonEncode(
+  //             {"userId": bucketId, "imgUrl": urlImage}))
+  //     .then((value) async {
+  //   final res = jsonDecode(value.body);
+  //   final imgRes = await http.get(Uri.parse(res["fileView"]));
+
+  //   final fileBlob = html.Blob([imgRes.bodyBytes]);
+  //   html.AnchorElement(href: html.Url.createObjectUrlFromBlob(fileBlob))
+  //     ..download = "${res["fileName"].toString().split('/').last}.png"
+  //     ..click();
+
+  //   //debugPrint(res.body);
+  // });
+}
--- lib/common/save_log_to_file_web.dart
+++ lib/common/save_log_to_file_web.dart
@@ -1,5 +1,8 @@
 // ignore: avoid_web_libraries_in_flutter
+import 'dart:convert';
+// ignore: avoid_web_libraries_in_flutter
 import 'dart:html' as html;
+import 'package:http/http.dart' as http;
 import 'dart:typed_data';
 import 'dart:async';
 
@@ -31,4 +34,31 @@ Future<List<int>> extractBytesFromFile(dynamic param) async {
   reader.readAsArrayBuffer(file);
 
   return completer.future;
+}
+
+Future<void> downloadImage(String urlImage, String bucketId, String mediaApiUrl) async {
+  // const fileName = "download.png";
+
+  // final res = await http.get(Uri.parse(urlImage));
+
+  // final fileBlob = Blob([res.bodyBytes]);
+  // AnchorElement(href: Url.createObjectUrlFromBlob(fileBlob))
+  //   ..download = fileName
+  //   ..click();
+
+  http
+      .post(Uri.parse("$mediaApiUrl/downloadAiImg"),
+          headers: {"Content-type": "application/json"},
+          body: jsonEncode({"userId": bucketId, "imgUrl": urlImage}))
+      .then((value) async {
+    final res = jsonDecode(value.body);
+    final imgRes = await http.get(Uri.parse(res["fileView"]));
+
+    final fileBlob = html.Blob([imgRes.bodyBytes]);
+    html.AnchorElement(href: html.Url.createObjectUrlFromBlob(fileBlob))
+      ..download = "${res["fileName"].toString().split('/').last}.png"
+      ..click();
+
+    //debugPrint(res.body);
+  });
 }
