6354e6f1cfb91723d8cebe04efd3f2f5d8ec8b81
skpark33 <skpark33333@gmail.com>
2024-11-28 16:55:09 +0900

    0.1.1

--- lib/common/windows_screenshot_io.dart
+++ lib/common/windows_screenshot_io.dart
@@ -1,37 +1,35 @@
-// import 'dart:async';
-// import 'dart:typed_data';
-// import 'dart:ui';
-// import 'package:hycop_multi_platform/hycop.dart';
-// import 'package:window_manager/window_manager.dart';
-// import 'package:screenshot/screenshot.dart';
+import 'dart:async';
+import 'dart:typed_data';
+import 'dart:ui';
+import 'package:hycop_multi_platform/hycop.dart';
+//import 'package:window_manager/window_manager.dart';
+import 'package:screenshot/screenshot.dart';
 
-// class WindowScreenshot {
-//   static final ScreenshotController screenshotController = ScreenshotController();
+class WindowScreenshot {
+  static final ScreenshotController screenshotController = ScreenshotController();
 
-//   // return screenshot image url
-//   static Future<String> uploadScreenshot(
-//       {required String bookId,
-//       Offset offset = Offset.zero,
-//       Size size = const Size(210, 150)}) async {
-//     try {
-//       // Capture the screenshot
-//       Uint8List? screenshotBytes = await screenshotController.capture(
-//         pixelRatio: window.devicePixelRatio,
-//         delay: Duration(milliseconds: 10),
-//       );
+  // return screenshot image url
+  static Future<String> uploadScreenshot(
+      {required String bookId,
+      Offset offset = Offset.zero,
+      Size size = const Size(210, 150)}) async {
+    try {
+      // Capture the screenshot
+      Uint8List? screenshotBytes = await screenshotController.capture(
+        //pixelRatio: window.devicePixelRatio,
+        delay: const Duration(milliseconds: 10),
+      );
 
-//       if (screenshotBytes != null) {
-//         // Upload the screenshot
-//         FileModel? result = await HycopFactory.storage!.uploadFile(
-//             '${bookId}_thumbnail.png', 'image/png', screenshotBytes);
+      // Upload the screenshot
+      FileModel? result = await HycopFactory.storage!
+          .uploadFile('${bookId}_thumbnail.png', 'image/png', screenshotBytes!);
 
-//         if (result != null) {
-//           return result.url;
-//         }
-//       }
-//       return Future.error('Failed to capture screenshot');
-//     } catch (e) {
-//       return Future.error('Error capturing screenshot: $e');
-//     }
-//   }
-// }
+      if (result != null) {
+        return result.url;
+      }
+      return Future.error('Failed to capture screenshot');
+    } catch (e) {
+      return Future.error('Error capturing screenshot: $e');
+    }
+  }
+}
